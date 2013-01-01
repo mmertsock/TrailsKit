a8c4dbb50f98533de170205748af39f36bde8130
Mike <mike@Mikes-MacBook-Pro.local>
2013-01-01 13:27:57 -0500

    Setting up Xcode project

---
+++ TrailsKit/TrailsKit.h
@@ -0,0 +1,13 @@
+//
+//  TrailsKit.h
+//  TrailsKit
+//
+//  Created by Mike Mertsock on 1/1/13.
+//  Copyright (c) 2013 Esker Apps. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface TrailsKit : NSObject
+
+@end
