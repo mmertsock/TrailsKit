c460d5ca799943c5f535f77d5ffd22d093b5aea7
mmertsock <mike.mertsock@gmail.com>
2013-01-19 10:41:26 -0500

    Split existing specs into parsing and mapping specs, and simplify both sets of specs.

--- TrailsKitTests/Parsers/TKGPXParserSpec.m
+++ TrailsKitTests/Parsers/TKGPXParserSpec.m
@@ -18,54 +18,78 @@ SPEC_BEGIN(TKGPXParserSpec)
 describe(@"TKGPXParser", ^{
 
     __block TKGPXParser* SUT;
-    __block TKGPXPolylineMapper* mapper;
-    __block NSData* dataToParse;
+    __block id<TKGPXOverlayMapper> mapper;
+    __block id mapperAsObject;
     
     beforeEach(^{
-        mapper = [[TKGPXPolylineMapper alloc] initWithStyle:[TKShapeStyle new]];
+        mapperAsObject = mapper = [KWMock mockForProtocol:@protocol(TKGPXOverlayMapper)];
         SUT = [[TKGPXParser alloc] initWithMapper:mapper];
     });
     
-	context(@"when parsing a valid GPX file with one track and one segment", ^{
+    context(@"when parsing a GPX file", ^{
+        
+        __block NSString* dataFileName;
         __block BOOL gotSuccess;
-        __block NSArray* gotPolylines;
+        __block BOOL gotFailure;
+        __block NSArray* gotOverlays;
+        __block void(^doMapping)(NSString*,NSArray*);
+        
         beforeEach(^{
-            gotSuccess = NO;
-            gotPolylines = nil;
-            //NSURL* url = [[NSBundle bundleForClass:[self class]] URLForResource:@"test_one_track" withExtension:@"gpx"];
-            //dataToParse = [NSData dataWithContentsOfURL:url];
-            dataToParse = loadData(@"test_one_track");
-            [SUT parseData:dataToParse
-                completion:^(BOOL success, NSArray *polylines) {
-                    gotSuccess = success;
-                    gotPolylines = polylines;
+            gotSuccess = gotFailure = NO;
+            gotOverlays = nil;
+            doMapping = ^(NSString* fileName, NSArray* overlaysToReturn) {
+                [mapperAsObject stub:@selector(mapOverlaysFromGPX:)
+                           andReturn:overlaysToReturn];
+                NSData* dataToParse = loadData(fileName);
+                [SUT parseData:dataToParse completion:^(BOOL success, NSArray *overlays) {
+                    if (success) gotSuccess = YES; else gotFailure = YES;
+                    gotOverlays = overlays;
                 }];
+            };
         });
-        it(@"should invoke the callback with the right data", ^{
-            [[expectFutureValue(theValue(gotSuccess)) shouldEventually] beTrue];
-            [[expectFutureValue(gotPolylines) shouldEventually] haveCountOf:1];
+    
+        context(@"when the GPX file is valid", ^{
+            beforeEach(^{ dataFileName = @"test_one_track"; });
+            
+            context(@"when the mapper returns overlays", ^{
+                beforeEach(^{
+                    doMapping(dataFileName, @[[TKStyledPolyline new], [TKStyledPolyline new]]);
+                });
+                it(@"should invoke the callback indicating success", ^{
+                    [[expectFutureValue(theValue(gotSuccess)) shouldEventually] beTrue];
+                });
+                it(@"should invoke the callback with the right data", ^{
+                    [[expectFutureValue(gotOverlays) shouldEventually] haveCountOf:2];
+                });
+            });
+            
+            context(@"when the mapper returns zero overlays", ^{
+                beforeEach(^{ doMapping(dataFileName, @[]); });
+                it(@"should invoke the callback indicating success", ^{
+                    [[expectFutureValue(theValue(gotSuccess)) shouldEventually] beTrue];
+                });
+                it(@"should invoke the callback with an empty set of overlays", ^{
+                    [[expectFutureValue(gotOverlays) shouldEventually] beEmpty];
+                });
+            });
+            
+            context(@"when the mapper returns nil", ^{
+                beforeEach(^{ doMapping(dataFileName, nil); });
+                it(@"should invoke the callback indicating failure", ^{
+                    [[expectFutureValue(theValue(gotFailure)) shouldEventually] beTrue];
+                });
+            });
         });
-        it(@"should set the style for the parsed polyline", ^{
-            [[expectFutureValue(gotPolylines[0]) shouldEventually] beKindOfClass:[TKStyledPolyline class]];
-            [[expectFutureValue([gotPolylines[0] shapeStyle]) shouldEventually] beIdenticalTo:mapper.shapeStyle];
+        
+        context(@"when the GPX file is invalid", ^{
+            beforeEach(^{ doMapping(@"test_invalid_gpx", @[]); });
+            it(@"should invoke the callback indicating failure", ^{
+                [[expectFutureValue(theValue(gotFailure)) shouldEventually] beTrue];
+            });
         });
+        
     });
     
-    context(@"when parsing a bad GPX file", ^{
-        __block BOOL gotFailure;
-        beforeEach(^{
-            // we will check that these get cleared out to NO/nil
-            gotFailure = NO;
-            dataToParse = loadData(@"test_invalid_gpx");
-            [SUT parseData:dataToParse
-                completion:^(BOOL success, NSArray *polylines) {
-                    gotFailure = !success;
-                }];
-        });
-        it(@"should invoke the callback with a failure", ^{
-            [[expectFutureValue(theValue(gotFailure)) shouldEventually] beTrue];
-        });
-    });
 });
 
 SPEC_END
---
+++ TrailsKitTests/Parsers/TKGPXPolylineMapperSpec.m
@@ -0,0 +1,70 @@
+//
+//  TKGPXPolylineMapperSpec.m
+//  TrailsKit
+//
+//  Created by Mike Mertsock on 1/19/13.
+//  Copyright (c) 2013 Esker Apps. All rights reserved.
+//
+
+#import <Kiwi/Kiwi.h>
+#import "TrailsKitGeometry.h"
+#import "TrailsKitParsers.h"
+#import <GPXParser/GPXParser.h>
+#import <MapKit/MapKit.h>
+
+SPEC_BEGIN(TKGPXPolylineMapperSpec)
+
+describe(@"TKGPXPolylineMapper", ^{
+    __block TKShapeStyle* shapeStyle;
+	__block TKGPXPolylineMapper* SUT;
+    __block GPX* gpx;
+    __block NSArray* result;
+    
+    beforeEach(^{
+        shapeStyle = [TKShapeStyle new];
+        SUT = [[TKGPXPolylineMapper alloc] initWithStyle:shapeStyle];
+        gpx = [GPX new];
+    });
+    
+    context(@"when initializing the mapper", ^{
+        it(@"should have the correct style", ^{
+            [[SUT.shapeStyle should] beIdenticalTo:shapeStyle];
+        });
+    });
+    
+	context(@"when mapping GPX with one track", ^{
+        beforeEach(^{
+            Track* track = [Track new];
+            track.path = [KWMock mockForProtocol:@protocol(MKOverlay)];
+            [gpx.tracks addObject:track];
+            result = [SUT mapOverlaysFromGPX:gpx];
+        });
+        specify(^{ [[result should] haveCountOf:1]; });
+        it(@"should return a correct Polyline overlay in the array", ^{
+            if (result.count < 1) return;
+            TKStyledPolyline* polyline = result[0];
+            [[polyline.shapeStyle should] beIdenticalTo:shapeStyle];
+            [[(id)polyline.overlay should] beIdenticalTo:[gpx.tracks[0] path]];
+        });
+    });
+    
+    context(@"when mapping GPX with multiple tracks", ^{
+        beforeEach(^{
+            Track* track = [Track new];
+            track.path = [KWMock mockForProtocol:@protocol(MKOverlay)];
+            [gpx.tracks addObject:track];
+            track = [Track new];
+            track.path = [KWMock mockForProtocol:@protocol(MKOverlay)];
+            [gpx.tracks addObject:track];
+            result = [SUT mapOverlaysFromGPX:gpx];
+        });
+        specify(^{ [[result should] haveCountOf:2]; });
+    });
+    
+    context(@"when mapping GPX with no tracks", ^{
+        beforeEach(^{ result = [SUT mapOverlaysFromGPX:gpx]; });
+        specify(^{ [[result should] beEmpty]; });
+    });
+});
+
+SPEC_END
